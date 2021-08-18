9ea32b197c042c5a89c6b0670534ecf3855c2648
Ketho <sghs17@outlook.com>
2021-08-18 22:25:37 +0200

    Added sanity check

--- LossControlTip.lua
+++ LossControlTip.lua
@@ -4,7 +4,7 @@ local tt = CreateFrame("GameTooltip", "LossControlTip", LossOfControlFrame, "Gam
 tt:SetScale(.9)
 
 LossOfControlFrame:HookScript("OnEvent", function(self, event)
-	if event == "LOSS_OF_CONTROL_ADDED" then
+	if event == "LOSS_OF_CONTROL_ADDED" and self.spellID then
 		tt:SetOwner(self, "ANCHOR_TOP")
 		tt:SetSpellByID(self.spellID)
 	end
